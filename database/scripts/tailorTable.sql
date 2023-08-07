8ed49aabf16a767fad1b3e0c947333317464e408
cnd007 <135818490+cnd007@users.noreply.github.com>
2023-08-07 22:10:57 +0530

    Update tailorTable.sql

---
+++ database/scripts/tailorTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tailor (
+  tailor_id INT PRIMARY KEY,
+  name VARCHAR(100),
+  shop_name VARCHAR(100),
+  customer_name VARCHAR(100),
+  customer_id INT,
+  customer_size VARCHAR(50)
+);
