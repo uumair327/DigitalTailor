a5a057ff298e9024169f2409453c11dea54c30ba
saibangude <99159223+saibangude@users.noreply.github.com>
2023-08-28 21:38:52 +0530

    Update tailorModel.js

---
+++ backend/models/tailorModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const tailorSchema = new mongoose.Schema({
+  // Define your tailor schema fields here
+  // For example:
+  name: String,
+  email: String,
+  // ... other fields
+});
+
+const Tailor = mongoose.model('Tailor', tailorSchema);
+
+module.exports = Tailor;
