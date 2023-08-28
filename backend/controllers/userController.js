22554b56532b749df3c2ae18103513de394046c2
saibangude <99159223+saibangude@users.noreply.github.com>
2023-08-28 21:47:22 +0530

    Update userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/userModel');
+
+exports.renderUserDashboard = (req, res) => {
+  // Your logic to render the user dashboard
+  res.render('user/dashboard', { pageTitle: 'User Dashboard' });
+};
+
+exports.submitMeasurements = (req, res) => {
+  // Your logic to handle submitted measurements
+  // Example: Save the measurements to the database
+  const measurements = req.body;
+  User.findByIdAndUpdate(req.user.id, { measurements })
+    .then(() => {
+      // Redirect back to the dashboard with a success message
+      res.redirect('/user/dashboard');
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    });
+};
