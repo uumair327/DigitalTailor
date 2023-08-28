7ca3cf97c6e658108d82f6a08e2357a4b168cb2e
saibangude <99159223+saibangude@users.noreply.github.com>
2023-08-28 21:39:20 +0530

    Update tailorController.js

--- backend/controllers/tailorController.js
+++ backend/controllers/tailorController.js
@@ -1,8 +1,34 @@
-// controllers/tailorController.js
-const path = require('path');
+const Tailor = require('../models/tailorModel');
 
-const renderTailorDashboard = (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'tailor_dashboard.html'));
+exports.renderTailorDashboard = (req, res) => {
+  // Your logic to render the tailor dashboard
+  res.render('tailor/dashboard', { pageTitle: 'Tailor Dashboard' });
 };
 
-module.exports = { renderTailorDashboard };
+exports.viewCustomerMeasurements = (req, res) => {
+  // Your logic to view and process customer measurement requests
+  // Example: Retrieve measurements from the database and render them
+  Tailor.findById(req.params.id)
+    .then(tailor => {
+      // Render tailor's measurement requests view
+      res.render('tailor/view_measurements', { tailor });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    });
+};
+
+exports.viewDesignRequests = (req, res) => {
+  // Your logic to view and process design requests
+  // Example: Retrieve design requests from the database and render them
+  Tailor.findById(req.params.id)
+    .then(tailor => {
+      // Render tailor's design requests view
+      res.render('tailor/view_designs', { tailor });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    });
+};
