65126436ecbd2d205736bbcd9fb361536121d336
saibangude <99159223+saibangude@users.noreply.github.com>
2023-08-28 21:40:58 +0530

    Update tailorRoutes.js

---
+++ backend/routes/tailorRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const tailorController = require('../controllers/tailorController');
+const router = express.Router();
+
+router.get('/dashboard', tailorController.renderTailorDashboard);
+router.get('/measurements/:id', tailorController.viewCustomerMeasurements);
+router.get('/designs/:id', tailorController.viewDesignRequests);
+
+module.exports = router;
