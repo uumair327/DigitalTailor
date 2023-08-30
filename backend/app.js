9e83563587be7e4d85b24df08ec9fee25337bbd9
Mohd Umair Ansari <72534352+uumair327@users.noreply.github.com>
2023-08-30 15:37:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,67 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes'); // Import your authentication routes
+// ...
+
 const app = express();
 
-// Body parsing middleware
-app.use(express.json());
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
-// Include models for different sections
-const userModel = require('./models/userModel');
-const tailorModel = require('./models/tailorModel');
-const designModel = require('./models/designModel');
+app.use(bodyParser.json());
 
-// Include routes for different sections
-app.use('/user', require('./routes/userRoutes'));
-app.use('/tailor', require('./routes/tailorRoutes'));
-app.use('/design', require('./routes/designRoutes'));
+// Use authentication routes
+app.use('/api/auth', authRoutes);
+// Include other routes as needed
 
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Email sending
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'your_email@gmail.com', // Your Gmail email address
+      pass: 'your_password' // Your Gmail password or an app-specific password
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'uumair327@gmail.com', // Recipient's email address
+    subject: 'New Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Email sent successfully');
+    }
+  });
+});
+
+// Serve the contact.html file
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
