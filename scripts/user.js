4d39e56ee1c84ec62e2240e759a7f0da527e0528
Mohd Umair Ansari <72534352+uumair327@users.noreply.github.com>
2023-09-06 15:31:15 +0530

    user

---
+++ scripts/user.js
@@ -0,0 +1,28 @@
+  // Get references to the gender select element and the Get Started button
+  const genderSelect = document.getElementById('gender');
+  const getStartedButton = document.getElementById('get-started-button');
+
+  // Add a click event listener to the Get Started button
+  getStartedButton.addEventListener('click', function (event) {
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Get the selected gender value
+    const selectedGender = genderSelect.value;
+
+    // Determine the URL based on the selected gender
+    let redirectUrl;
+
+    if (selectedGender === 'female') {
+      redirectUrl = 'femaleuser.html';
+    } else if (selectedGender === 'male') {
+      redirectUrl = 'maleuser.html';
+    } else {
+      // Handle 'other' or other cases if needed
+      redirectUrl = 'otheruser.html';
+    }
+
+    // Redirect the user to the appropriate page
+    window.location.href = redirectUrl;
+  });
+
