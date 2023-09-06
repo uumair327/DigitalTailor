e3960652acd64c53acd7e10d31d21d9a087e574e
cnd007 <135818490+cnd007@users.noreply.github.com>
2023-09-06 15:54:20 +0530

    Update tailorTable.sql

--- database/scripts/tailorTable.sql
+++ database/scripts/tailorTable.sql
@@ -1,10 +1,41 @@
-{
-  "_id": ObjectId("tailor_id"),
-  "name": "John Taylor",
-  "email": "john@example.com",
-  "phone": "123-456-7890",
-  "address": "789 Sewing St, City",
-  "specialties": ["Suits", "Dresses", "Alterations"],
-  // other tailor details
-}
+const MongoClient = require('mongodb').MongoClient;
+
+// MongoDB Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'tailor_management';
+
+// Collection Names
+const tailorsCollection = 'tailors';
+const ordersCollection = 'orders';
+const customersCollection = 'customers';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to MongoDB
+client.connect(async (err) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
+
+  console.log('Connected to MongoDB');
+
+  const db = client.db(dbName);
+
+  // Create collections to store tailor, order, and customer data
+  await db.createCollection(tailorsCollection);
+  await db.createCollection(ordersCollection);
+  await db.createCollection(customersCollection);
+
+  console.log(`Collections created: ${tailorsCollection}, ${ordersCollection}, ${customersCollection}`);
+
+  // You can now interact with these collections to manage tailors, orders, and customers
+
+  // Close the MongoDB connection
+  client.close();
+});
+
 
