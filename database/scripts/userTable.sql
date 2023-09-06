06a8121075c5b4008dadbe4c3fcaaecf464a36b8
cnd007 <135818490+cnd007@users.noreply.github.com>
2023-09-06 15:53:36 +0530

    Update userTable.sql

--- database/scripts/userTable.sql
+++ database/scripts/userTable.sql
@@ -4,16 +4,18 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'userdb';
+const dbName = 'tailor_management';
 
-// Collection Name
-const collectionName = 'users';
+// Collection Names
+const usersCollection = 'users';
+const tailorsCollection = 'tailors';
+const ordersCollection = 'orders';
 
 // Create a new MongoClient
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Connect to MongoDB
-client.connect((err) => {
+client.connect(async (err) => {
   if (err) {
     console.error('Error connecting to MongoDB:', err);
     return;
@@ -23,22 +25,19 @@ client.connect((err) => {
 
   const db = client.db(dbName);
 
-  // Create a collection to store user data
-  db.createCollection(collectionName, (err, collection) => {
-    if (err) {
-      console.error('Error creating collection:', err);
-      return;
-    }
+  // Create collections to store user, tailor, and order data
+  await db.createCollection(usersCollection);
+  await db.createCollection(tailorsCollection);
+  await db.createCollection(ordersCollection);
 
-    console.log(`Collection "${collectionName}" created`);
+  console.log(`Collections created: ${usersCollection}, ${tailorsCollection}, ${ordersCollection}`);
 
-    // You can now interact with the "users" collection
-    // Insert, query, update, or delete user data here
+  // You can now interact with these collections to manage users, tailors, and orders
 
-    // Close the MongoDB connection
-    client.close();
-  });
+  // Close the MongoDB connection
+  client.close();
 });
+
 
 
 
