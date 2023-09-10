cb63dc2c32aa12467be75277adc9078f5695d590
Mohd Umair Ansari <72534352+uumair327@users.noreply.github.com>
2023-09-10 16:58:17 +0530

    Add files via upload

--- scripts/auth.js
+++ scripts/auth.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const username = document.getElementById('username').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
-        const confirmPassword = document.getElementById('confirm-password').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
 
         if (password !== confirmPassword) {
             alert("Passwords do not match!");
